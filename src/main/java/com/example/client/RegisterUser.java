627dc997635111389c72f08beea08433057b17c6
TsvetanG <tsvetan.georgiev@gmail.com>
2017-11-01 23:03:56 -0400

    add register

---
+++ src/main/java/com/example/client/RegisterUser.java
@@ -0,0 +1,16 @@
+package com.example.client;
+
+public class RegisterUser {
+  
+  public static void main(String[] args) {
+    String org = null;
+    String userName = null;
+    new RegisterUser().registerAndEnroll(org, userName);
+  }
+
+  protected void registerAndEnroll(String org, String userName) {
+    
+    
+  }
+
+}
